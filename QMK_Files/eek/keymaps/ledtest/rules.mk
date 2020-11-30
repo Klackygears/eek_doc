6f5975ae7eedda93eebc742d3759e72e5bf41980
Klackygears <37446827+Klackygears@users.noreply.github.com>
2020-11-29 23:02:40 -0500

    corrections and updates to qmk firmware regarding rgb leds and some pin issues

--- QMK_Files/eek/keymaps/ledtest/rules.mk
+++ QMK_Files/eek/keymaps/ledtest/rules.mk
@@ -1,3 +1,2 @@
-
 RGBLIGHT_ENABLE = yes        # Enable keyboard RGB underglow
-RGB_MATRIX_ENABLE = no       # Enable for pretty RGB matrix effects
+#RGB_MATRIX_ENABLE = WS2812       # Enable for pretty RGB matrix effects
